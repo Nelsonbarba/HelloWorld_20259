0447f6e2699e51b25e8ec29c21a269172e578dca
unknown <sanchezoziel659@gmail.comgit config --global user name Nelson>
2025-10-28 17:35:42 -0600

    Primer Commit

---
+++ index.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IDS2_proyecto
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bt_Calcular_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(this.txt_X.Text);
+
+            double a = 2;
+            double b = 2;
+            double c = 2;
+            double y = 0;
+
+            // y = (a)x^2 + b(x) + c 
+            y = ((a) * (Math.Pow(x, 2))) + (b * x) + c;
+
+            this.txt_y.Text = y.ToString();
+        }
+    }
+}
---
+++ index.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace IDS2_proyecto
+{
+
+
+    public partial class index
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txt_X.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_X;
+
+        /// <summary>
+        /// Control bt_Calcular.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bt_Calcular;
+
+        /// <summary>
+        /// Control txt_y.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_y;
+    }
+}
